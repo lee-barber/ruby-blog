0413c7932e8d2519b3f954adc35bab5fca6bc034
Aaron Brown <ayayron44@gmail.com>
2018-02-15 11:41:32 -0500

    basic sign up functionality

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ set :database, "sqlite3:main.sqlite3"
 get '/' do
 	erb :index
 end
+
+get '/signup' do
+	erb :signup
+end
+
+post '/signup' do
+	User.create(fname: params[:fname], lname: params[:lname], username: params[:username], password: params[:password], email: params[:email])
+	redirect '/'
+end
